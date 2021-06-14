f088f04de79cf6f6a696c111dcea8b9b7fd00624
Shinigami1312 <63165149+Shinigami1312@users.noreply.github.com>
2021-06-14 21:49:59 +0530

    Initial commit

---
+++ database/bank_db.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2021 at 11:34 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bank_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transfer`
+--
+
+CREATE TABLE `transfer` (
+  `sid` int(255) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `receiver` varchar(255) NOT NULL,
+  `amount` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transfer`
+--
+
+INSERT INTO `transfer` (`sid`, `sender`, `receiver`, `amount`) VALUES
+(1, 'Jayraj', 'Shreya', 4000),
+(2, 'Jayraj', 'Abhi', 5000),
+(3, 'Abhi', 'Jayraj', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `balance` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Jayraj', 'jayraj@gmail.com', 72000),
+(2, 'Abhi', 'abhi@gmail.com', 9000),
+(3, 'Payal', 'payal@gmail.com', 50000),
+(4, 'Avinash', 'avinash@gmail.com', 30000),
+(5, 'Iswarya', 'iswarya@gmail.com', 20000),
+(6, 'Darshil', 'darshil@gmail.com', 23430),
+(7, 'Shreya', 'shreya@gmail.com', 74000),
+(8, 'Yash', 'yash@gmail.com', 75000),
+(9, 'Ramesh', 'ramesh@gmail.com', 30000),
+(10, 'Suresh', 'suresh@gmail.com', 31000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transfer`
+--
+ALTER TABLE `transfer`
+  MODIFY `sid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-fixed-top " style="background-color:#ffeacf;">
+<div class="container">
+<div class="navbar-header">
+<button class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" style="color: #393e46;">Bank of Sparks Foundation</a>
+</div>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="nav navbar-nav navbar-right" >
+<li><a href = "index.php" style="color: #393e46;"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
+<li><a href="user.php" style="color: #393e46;"><span class="glyphicon glyphicon-user"></span> New Users</a></li>
+<li><a href="transaction.php" style="color: #393e46;"><span class="glyphicon glyphicon-transfer"></span> Money Transfer</a></li>
+<li><a href="history.php" style="color: #393e46;"><span class="glyphicon glyphicon-inbox"></span> Transfer Logs</a></li>
+</ul>
+</div>
+</div>
+</nav>
